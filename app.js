e9c60433ad44ee1c1e8ab29c63628baadcb89113
Shaun Wahle <shaun.wahle@gmail.com>
2017-05-16 09:09:09 -0700

    Updated HTML and CSS

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var cooksShopSeaTac = {
   minCust: 3,
   maxCust: 24,
@@ -11,6 +10,7 @@ var cooksShopSeaTac = {
   //than 0 it will keep counting down by 1 everytime. This is accomplished by using math.floor and math random
   randomCust: function() {
     for( var i = 15; i > 0; i--) {
+      //Here I'm calling to math functions to pull a random number and round to the nearest whole number also setting parameters to keep it between max/min
       this.totalCustEachHour.push ((Math.floor (Math.random() * (this.maxCust - this.minCust + 1))) + this.minCust);
     }
     console.log(this.totalCustEachHour);
@@ -41,6 +41,10 @@ var cooksShopCapHill = {
       this.totalCustEachHour.push ((Math.floor (Math.random() * (this.maxCust - this.minCust + 1))) + this.minCust);
     }
     console.log(this.totalCustEachHour);
+  },
+  printCookies: function() {
+
+
   }
 };
 
